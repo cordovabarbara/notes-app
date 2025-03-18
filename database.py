d0da4cc07e4053a34b899d313adcf87c8bb2e77c
cordovabarbara <aliendocordova@gmail.com>
2025-03-17 19:15:42 -0500

    fix:corregir nombre de la funcion para la creacion de la tabla

--- database.py
+++ database.py
@@ -1,4 +1,21 @@
 import sqlite3
 
-def conecta_db():
-    return sqlite3.connect('notes.db')
+DATABASE_NAME = "notes.db"
+
+def conect_db():
+    return sqlite3.connect(DATABASE_NAME)
+
+def create_table():
+    with conect_db() as conn:
+        conn.execute('''
+            CREATE TABLE IF NOT EXISTS notes (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT NOT NULL,
+                content TEXT NOT NULL
+            )''')
+def new_note():
+    with conect_db() as conn:
+        conn.execute
+
+
+create_table()
