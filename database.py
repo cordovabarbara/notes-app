77505a0ac82323c41b081447fe08bbe9a053a8b8
cordovabarbara <aliendocordova@gmail.com>
2025-03-21 11:02:48 -0500

    feat: agregar función update_note() para actualizar titulo o content o ambos

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 DATABASE_NAME = "notes.db"
 
 def conect_db():
@@ -15,6 +16,7 @@ def create_table():
             )''')
         conn.commit()
 
+#crea una nueva nota
 def new_note(title, content):
     with conect_db() as conn:
         conn.execute('''
@@ -27,7 +29,7 @@ def new_note(title, content):
 create_table()
 
 #agregar nota
-#new_note("Mi primera nota", "Este es el contenido de mi primera nota.")
+#new_note("Mi segunda nota", "Lista a seguir para programar en Python.")
 
 #Funcion para tener todas la notas
 def get_notes():
@@ -36,8 +38,8 @@ def get_notes():
         query = 'SELECT * FROM notes'
         cursor.execute(query)
         return cursor.fetchall()
-    
-#Funcion para tener todas la notas por id
+
+#Funcion para tener la notas por id
 def get_note_by_id(id):
     with conect_db() as conn:
         cursor = conn.cursor()
@@ -48,8 +50,6 @@ def get_note_by_id(id):
         return f"No se encontro una nota con el ID {id}"
     return note
 
-#print(get_note_by_id(100))
-
 #Funcion para eliminar notar por id
 def delete_note(id):
     with conect_db() as conn:
@@ -57,6 +57,45 @@ def delete_note(id):
         query = 'DELETE FROM notes WHERE id =?'
         cursor.execute(query, (id,))
         if cursor.rowcount == 0:
-            return f"No existe una nota con el id{id}"
+            return f"No existe una nota con el id {id}"
         conn.commit()
-        return f"Nota con ID {id} eliminada exitosamente"
+        return f"La Nota con ID {id} eliminada exitosamente"
+
+#Funcion para actualizar nota, ya sea titulo o contenido o ambos
+def update_note(id, title=None, content=None):
+    try:
+        with conect_db() as conn:
+            cursor = conn.cursor()
+
+# Verificar si el ID existe antes de actualizar
+        cursor.execute("SELECT id FROM notes WHERE id = ?", (id,))
+        if cursor.fetchone() is None:
+                return f"No existe una nota con el ID {id}"
+        
+        update_fields = []
+        values = []
+
+        if title:
+            update_fields.append("title = ?")
+            values.append(title)
+        if content:
+            update_fields.append("content = ?")
+            values.append(content)
+        
+        if not update_fields:
+            return "No se proporcionaron valores para actualizar."
+
+        query = f'''
+                UPDATE notes
+                SET {", ".join(update_fields)}
+                WHERE id = ?
+                '''
+        values.append(id)
+
+        cursor.execute(query, values)
+        conn.commit()
+        return f"La Nota con ID {id} fue actualiza exitosamente" 
+
+    except sqlite3.Error as e:
+        return f"Error al actualizar la nota: {e}"   
+        
